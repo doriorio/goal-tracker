d0939831eb3461b6beb44d5aff1aa9e3b3850daa
Marquiese Solomon <blaksnoopy@Marquieses-MacBook-Pro.local>
2019-12-18 14:36:49 -0800

    index page created

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
    path('', views.home, name='home'),
+   path('resolutions/', views.resoltions_index, name='index'),
    path('accounts/signup/', views.signup, name='signup'),
 ]
