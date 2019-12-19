636f4148eeb0f1b3c10ba1c4b6e060b974f4f7d3
SpiffyTheSpaceman <jameskim056@gmail.com>
2019-12-19 15:44:58 -0800

    Update profile page

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Resolution
+from .models import Resolution, Comment
 
 # Register your models here.
 admin.site.register(Resolution)
+admin.site.register(Comment)
--- main_app/urls.py
+++ main_app/urls.py
@@ -5,9 +5,9 @@ urlpatterns = [
    path('', views.home, name='home'),
    path('resolutions/', views.ResolutionIndex.as_view(), name='index'),
    path('resolutions/<int:pk>', views.ResolutionDetail.as_view(), name='detail'),
+   path('resolutions/<int:pk>', views.ResolutionDetail.as_view(), name='detail'),
    path('resolutions/create/', views.ResolutionCreate.as_view(), name='create'),
    path('resolutions/<int:resolution_id>/add_comment/', views.add_comment, name='add_comment'),
-
    path('resolutions/<int:pk>/update/', views.ResolutionUpdate.as_view(), name="update"),
    path('resolutions/<int:pk>/delete/', views.ResolutionDelete.as_view(), name="delete"),
    path('accounts/signup/', views.signup, name='signup'),
