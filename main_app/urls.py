753650c1ddd02925d706b4a5fdc5bf93a710d0e2
SpiffyTheSpaceman <jameskim056@gmail.com>
2019-12-20 11:39:12 -0800

    Create CRUD for entries

--- main_app/urls.py
+++ main_app/urls.py
@@ -11,5 +11,9 @@ urlpatterns = [
    path('resolutions/<int:comment_id>/delete_comment/', views.delete_comment, name='delete_comment'),
    path('resolutions/<int:pk>/update/', views.ResolutionUpdate.as_view(), name="update"),
    path('resolutions/<int:pk>/delete/', views.ResolutionDelete.as_view(), name="delete"),
+   path('resolutions/<int:resolution_id>/entries/', views.EntryList.as_view(), name="entry_index"),
+   path('resolutions/<int:resolution_id>/entries/create/', views.EntryCreate.as_view(), name="entry_create"),
+   path('entries/<int:entry_id>/delete_entry/', views.delete_entry, name='delete_entry'),
+   path('entries/<int:pk>/update_entry/', views.EntryUpdate.as_view(), name='update_entry'),
    path('accounts/signup/', views.signup, name='signup'),
 ]
